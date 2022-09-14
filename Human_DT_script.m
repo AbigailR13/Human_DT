4b7d1dbb59c1a82ffd6cc6f9ce6a12366cdf78c8
AbigailR13 <90467889+AbigailR13@users.noreply.github.com>
2022-09-14 00:09:14 -0400

    Update Human_DT_script.m

--- Human_DT_script.m
+++ Human_DT_script.m
@@ -8,7 +8,7 @@ data{1} = ["24"];
 data{2} = ["25"];
 
 % input data into human DT
-[percent_correct,physiological_states,qofm] = Human_DT(data);
+[percent_correct,physiological_states,qofo] = Human_DT(data);
 
 percent_correct
 
